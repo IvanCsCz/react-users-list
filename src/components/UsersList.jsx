05b984816764b3f2465c597f1b87591ba6b57e89
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-04-30 17:07:21 -0600

    added search input

--- src/components/UsersList.jsx
+++ src/components/UsersList.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react';
 import UserRow from './UserRow';
 import style from './UsersList.module.css';
 
 const UsersList = ({ users, children }) => {
+	const [search, setSearch] = useState('jo');
+
+	const normalizedSearch = search.toLowerCase();
+
+	const usersFiltered = search
+		? users.filter((user) =>
+				user.name.toLowerCase().startsWith(normalizedSearch)
+		  )
+		: users;
+
 	const usersRendered =
-		users.length > 0 ? (
-			users.map(({ id, name, active, role }) => (
+		usersFiltered.length > 0 ? (
+			usersFiltered.map(({ id, name, active, role }) => (
 				<UserRow key={id} name={name} active={active} role={role} />
 			))
 		) : (
@@ -14,6 +25,21 @@ const UsersList = ({ users, children }) => {
 	return (
 		<div className={style.list}>
 			<h1>Lista de usuarios</h1>
+			<input
+				type='text'
+				value={search}
+				onChange={(ev) => setSearch(ev.target.value)}
+			/>
+			{/* Formato no controlado
+			<form
+				onSubmit={(ev) => {
+					ev.preventDefault();
+					setSearch(ev.target.search.value);
+				}}
+			>
+				<input type='text' name='search'></input>
+				<button type='submit'>Buscar</button>
+			</form> */}
 			{usersRendered}
 		</div>
 	);
