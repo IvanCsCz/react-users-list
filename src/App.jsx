34621b39bed08cd32e785534b45d7fc7c53efe56
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-05-07 14:42:09 -0600

    Users data from API

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,5 @@
 import UsersList from './components/UsersList';
-import { USER_ROLES } from './constants/userRoles';
 
-const USERS = [
-	{
-		username: 'Luis',
-		name: 'Luis Caudillo Torres',
-		active: true,
-		role: USER_ROLES.STUDENT
-	},
-	{
-		username: 'Yola',
-		name: 'Yolanda Ramos Ramos',
-		active: true,
-		role: USER_ROLES.TEACHER
-	},
-	{
-		username: 'Gus',
-		name: 'Gustavo Aguayo Yañez',
-		active: true,
-		role: USER_ROLES.STUDENT
-	},
-	{
-		username: 'Vale',
-		name: 'Valeria Dominguez',
-		active: false,
-		role: USER_ROLES.STUDENT
-	},
-	{
-		username: 'Rona',
-		name: 'Oscar Ronaldo Rodriguez Vega',
-		active: true,
-		role: USER_ROLES.TEACHER
-	},
-	{
-		username: 'Ivan',
-		name: 'José Iván Campos Chavez',
-		active: true,
-		role: USER_ROLES.OTHER
-	}
-];
-
-const App = () => <UsersList initialUsers={USERS} />;
+const App = () => <UsersList />;
 
 export default App;
--- src/lib/hooks/useFilters.js
+++ src/lib/hooks/useFilters.js
@@ -7,7 +7,7 @@ export const useFilters = () => {
 		onlyActive: false,
 		sortBy: SORT_OPTIONS.DEFAULT,
 		page: 1,
-		itemsPerPage: 2
+		itemsPerPage: 6
 	});
 
 	const setSearch = (search) =>
@@ -20,6 +20,7 @@ export const useFilters = () => {
 	const setSortBy = (sortBy) =>
 		setFilters({
 			...filters,
+			page: 1,
 			sortBy
 		});
 
@@ -46,6 +47,7 @@ export const useFilters = () => {
 	const setItemsPerPage = (itemsPerPage) => {
 		setFilters({
 			...filters,
+			page: 1,
 			itemsPerPage
 		});
 	};
