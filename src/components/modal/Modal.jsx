5ef7c4cbb964ad1b561ff4647328ad0c25bfe4b2
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-05-18 01:05:56 -0600

    Handle click outside modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,25 +1,35 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import IconButtton from '../buttons/IconButton';
 import XMarkIcon from '../icons/XMarkIcon';
 import style from './Modal.module.css';
 
 const Modal = ({ closeModal, children }) => {
+	const modalRef = useRef(null);
+
 	useEffect(() => {
 		if (!children) return;
 
 		document.body.classList.add(style.bodyOverflow);
 
+		const handleClickOutside = (ev) => {
+			if (!modalRef.current.contains(ev.target)) closeModal();
+		};
+		document.addEventListener('click', handleClickOutside, { capture: true });
+
 		return () => {
 			document.body.classList.remove(style.bodyOverflow);
+			document.removeEventListener('click', handleClickOutside, {
+				capture: true
+			});
 		};
-	}, [children]);
+	}, [children, closeModal]);
 
 	if (!children) return null;
 
 	return createPortal(
 		<div className={style.overlay}>
-			<div className={style.modal}>
+			<div ref={modalRef} className={style.modal}>
 				<IconButtton
 					className={style.close}
 					icon={XMarkIcon}
