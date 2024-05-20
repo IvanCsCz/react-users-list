3f0571e4c968f3b4569bd3008b681f34aa88af0a
Jose Ivan Campos Chavez <ivan820plus@hotmail.com>
2024-05-20 13:44:32 -0600

    Refactor UserDisplay

--- src/components/user/UserDisplay.jsx
+++ src/components/user/UserDisplay.jsx
@@ -1,9 +1,16 @@
 import style from './UserDisplay.module.css';
 
-const UserDisplay = ({ name, username }) => (
+const UserDisplay = ({ name, username, picture }) => (
 	<div className={style.wrapper}>
-		<span>{name}</span>
-		<span className={style.username}>@{username}</span>
+		<img
+			className={style.picture}
+			src={picture || '/user-pic.svg'}
+			alt={`Foto de ${name}`}
+		/>
+		<div className={style.display}>
+			<span>{name}</span>
+			<span className={style.username}>@{username}</span>
+		</div>
 	</div>
 );
 
